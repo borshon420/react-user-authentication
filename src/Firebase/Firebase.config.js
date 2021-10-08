1321360b5660efab984a8a4df007823a06b490c2
borshon420 <borshonarafat420@gmail.com>
2021-10-08 16:23:00 +0600

    react-user-authentication

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAot9tPnW9nIcAFZvqlqQNcEEbugmngWfM",
+    authDomain: "react-firebase-authentic-33a6c.firebaseapp.com",
+    projectId: "react-firebase-authentic-33a6c",
+    storageBucket: "react-firebase-authentic-33a6c.appspot.com",
+    messagingSenderId: "558246361582",
+    appId: "1:558246361582:web:75a6d281ecbfef1ec376ff"
+  };
+
+  export default firebaseConfig;
